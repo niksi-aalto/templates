d81951c27005e12a17eacdef3c39710a5d6daded
Luukas Pörtfors <luukas.portfors@aalto.fi>
2024-11-01 17:48:42 +0200

    chore: use flake-utils

--- scala/flake.nix
+++ scala/flake.nix
@@ -2,42 +2,38 @@
   description = "A Scala devcontainer flake";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.niksi-devcontainer.url = "git+ssh://git@version.aalto.fi/niksi/niksi-devcontainer";
 
   outputs = {
-    self,
     nixpkgs,
+    flake-utils,
     niksi-devcontainer,
-  }: let
-    supportedSystems = ["x86_64-linux" "x86_64-darwin"];
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = niksi-devcontainer.lib.mkDevcontainer {
+          inherit pkgs;
+          name = "scala";
+          paths = with pkgs; [
+            bash
+            coreutils-full
+            scala_3
+            git
+            sbt
+            metals
+            bloop
+            coursier
+            jdk
 
-    forallSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-    nixpkgsFor = forallSystems (system: import nixpkgs {inherit system;});
-  in {
-    packages = forallSystems (system: let
-      pkgs = nixpkgsFor.${system};
-    in {
-      default = niksi-devcontainer.lib.mkDevcontainer {
-        inherit pkgs;
-        name = "niksi-scala";
-        paths = with pkgs; [
-          bash
-          coreutils-full
-          scala_3
-          git
-          sbt
-          metals
-          bloop
-          coursier
-          jdk
-
-          xorg.libX11
-          xorg.libXext
-          xorg.libXtst
-        ];
-      };
-    });
-  };
+            xorg.libX11
+            xorg.libXext
+            xorg.libXtst
+          ];
+        };
+      }
+    );
 }
