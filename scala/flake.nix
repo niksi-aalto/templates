17134af9f206bebdd202981038f72f889a4e4da8
Luukas Pörtfors <luukas.portfors@aalto.fi>
2024-11-22 15:09:44 +0200

    feat: load paths.nix file

--- scala/flake.nix
+++ scala/flake.nix
@@ -14,25 +14,28 @@
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
+        paths = pkgs.lib.optional (builtins.pathExists ./paths.nix) (builtins.head (import ./paths.nix {inherit pkgs;}));
       in {
         packages.default = niksi-devcontainer.lib.mkDevcontainer {
           inherit pkgs;
           name = "scala";
-          paths = with pkgs; [
-            bash
-            coreutils-full
-            scala_3
-            git
-            sbt
-            metals
-            bloop
-            coursier
-            jdk
+          paths = with pkgs;
+            [
+              bash
+              coreutils-full
+              scala_3
+              git
+              sbt
+              metals
+              bloop
+              coursier
+              jdk
 
-            xorg.libX11
-            xorg.libXext
-            xorg.libXtst
-          ];
+              xorg.libX11
+              xorg.libXext
+              xorg.libXtst
+            ]
+            ++ paths;
         };
       }
     );
